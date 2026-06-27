7bbb506c17abc9d79a7341ac8850f9fe2fb6e189
ursaworks <eeeaaason070311@gmail.com>
2026-06-27 15:11:21 -0500

    change autostart

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,9 @@
-sleep 5
-cd ~/Desktop/sp_vision_25/
-screen \
-    -L \
-    -Logfile logs/$(date "+%Y-%m-%d_%H-%M-%S").screenlog \
-    -d \
-    -m \
-    bash -c "./watchdog.sh"
+sleep 10
+cd ~/Vision26
+# 最多等 60 秒，直到海康相机出现在 USB 上
+for i in $(seq 1 60); do
+    lsusb | grep -qi 2bdf && break
+    sleep 1
+done
+mkdir -p logs
+screen -S sp_vision -L -Logfile logs/$(date "+%Y-%m-%d_%H-%M-%S").screenlog -d -m bash -c "./watchdog.sh"
